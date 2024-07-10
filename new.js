c6b95894572417e028ddf87ad2db40f29679717c
dohun08 <bemik624@gmail.com>
2024-07-10 17:53:52 +0900

    3

--- new.js
+++ new.js
@@ -204,28 +204,387 @@ function gg3(){
 }
 
 
-var month = document.querySelector("#month");
-var year = document.querySelector("year");
-function date(){
-    const current = parseInt(month.textContent);
-    if(month.value == 7){
-        document.querySelector("#num1").style.background="#4DB02C";
-    }
-}
+
+var number =7;
+var work = document.querySelector(".work");
+var date31 = document.querySelector("#num31");
 function plus(){
-    var num = 0;
     const current = parseInt(month.textContent);
+    
     if(current + 1 > 12){
-        month.textContent = current - 11 + "월";
+        month.textContent = current + "월";
     }
-    else
+    else{
         month.textContent = current + 1 + "월";
+        number +=1;
+        if(number ==1 || number == 3 || number == 5 || number == 7 || number==8 || number ==10 || number==12) date31.style.display = "block";
+        else date31.style.display = "none";
+        if(number == 1){
+            for($j=1; $j<=31; $j++)
+                document.getElementById("num"+$j).style.background = "none";
+            work.innerHTML = "";
+            work.innerHTML += "<li>01.01  신정</li>";
+            work.innerHTML += "<li>01.09  종업식,졸업식</li>";
+        }
+        if(number == 2){
+            for($j=1; $j<=31; $j++){
+                if($j>8 && $j<13) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }   
+            work.innerHTML = "";
+            work.innerHTML += "<li>02.09  설날연휴</li>";
+            work.innerHTML += "<li>02.10  설날</li>";
+            work.innerHTML += "<li>02.11  설날연휴</li>";
+            work.innerHTML += "<li>02.12  대체휴일</li>";
+        }
+        if(number == 3){
+            for($j=1; $j<=31; $j++){
+                if($j==1 || $j==2 || $j==4 || $j==9 || $j==16 || $j==23 || $j==30) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+                
+            work.innerHTML = "";
+            for($i=1; $i<=9; $i++){
+                if($i == 2 || $i==9) work.innerHTML += "<li>03.0"+$i+"  토요휴업일</li>";
+                if($i==1) work.innerHTML += "<li>03.0"+$i+"  3.1절</li>";
+                if($i==4) work.innerHTML += "<li>03.0"+$i+"  입학식</li>";
+            }
+            for($i=10; $i<=31; $i++){
+                if($i==16 || $i==23 || $i==30) work.innerHTML += "<li>03."+$i+"  토요휴업일</li>";
+            }
+        }
+        if(number == 4){
+            for($j=1; $j<=31; $j++){
+                if($j==6 || $j>8 && $j<12 || $j==13 || $j==20 || $j>24 && $j<28) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+            work.innerHTML = "";
+            work.innerHTML += "<li>04.06  토요휴업일</li>";
+            work.innerHTML += "<li>04.09  1학년 영어듣기평가</li>";
+
+            for($i=10; $i<=31; $i++){
+                if($i==11) work.innerHTML += "<li>04."+$i+"  2학년 영어듣기평가</li>";
+                if($i==13 || $i==20 || $i==27) work.innerHTML += "<li>04."+$i+"  토요휴업일</li>";
+                if($i==24 || $i==25) work.innerHTML += "<li>04."+$i+"  중간고사</li>";
+            }
+        }
+        if(number == 5){
+            for($j=1; $j<=31; $j++){
+                if($j>3 && $j<7 || $j==11 || $j==15 || $j==18 || $j>19 && $j<26) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+            work.innerHTML = "";
+            work.innerHTML += "<li>05.04  토요휴업일</li>";
+            work.innerHTML += "<li>05.05  어린이날</li>";
+            work.innerHTML += "<li>05.06  대체휴일</li>";
+            for($i=10; $i<=31; $i++){
+                if($i==15) work.innerHTML += "<li>07."+$i+"  석가탄신일(부처님오신날)</li>";
+                if($i==11 || $i==18 || $i==25) work.innerHTML += "<li>05."+$i+"  토요휴업일</li>";
+                if($i>=20 && $i<=25) work.innerHTML += "<li>05."+$i+"  현장체험학습</li>";
+            }
+        }
+        if(number == 6){
+            for($j=1; $j<=31; $j++){
+                if($j==1 || $j>5 && $j<9 || $j==15 || $j==22 || $j==29) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+                
+            work.innerHTML = "";
+                if($i == 1) work.innerHTML += "<li>06.01  토요휴업일</li>";
+                if($i==6) work.innerHTML += "<li>06.06  현충일</li>";
+                if($i==7) work.innerHTML += "<li>06.07  개교기념일</li>";
+                if($i==8) work.innerHTML += "<li>06.08  토요휴업일</li>";
+            for($i=10; $i<=31; $i++){
+                if($i==15 || $i==22 || $i==19) work.innerHTML += "<li>06."+$i+"  토요휴업일</li>";
+            }
+        }
+        if(number == 7){
+            for($j=1; $j<=31; $j++){
+                if($j>0 && $j<4 || $j==6 || $j==10 || $j==13 || $j==20 || $j==19 || $j>21 && $j<32) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+                
+            work.innerHTML = "";
+            for($i=1; $i<=9; $i++){
+                if($i <= 3) work.innerHTML += "<li>07.0"+$i+"  기말고사</li>";
+                if($i==6) work.innerHTML += "<li>07.0"+$i+"  토요휴업일</li>";
+            }
+            for($i=10; $i<=31; $i++){
+                if($i==10) work.innerHTML += "<li>07."+$i+"  직업기초능력평가(3학년)</li>";
+                if($i==13 || $i==20 || $i==27) work.innerHTML += "<li>07."+$i+"  토요휴업일</li>";
+                work.innerHTML += "<li>07."+$i+"  여름방학</li>";
+            }
+        }
+        if(number == 8){
+            for($j=1; $j<=31; $j++){
+                if($j>0 && $j<18 || $j==19 || $j==24 || $j==31) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+                
+            work.innerHTML = "";
+            for($i=1; $i<=9; $i++){
+                work.innerHTML += "<li>08.0"+$i+"  여름방학</li>";
+                if($i == 3) work.innerHTML += "<li>08.0"+$i+"  토요휴업일</li>";
+            }
+            for($i=10; $i<=31; $i++){
+                if($i == 10 || $i==17 || $i==24 || $i==31) work.innerHTML += "<li>08."+$i+"  토요휴업일</li>";
+                if($i == 15) work.innerHTML += "<li>08."+$i+"  광복절</li>";
+                if($i<19) work.innerHTML += "<li>08."+$i+"  여름방학</li>";
+                if($i==19) work.innerHTML += "<li>08."+$i+"  개학식</li>";
+            }
+        }
+        if(number == 9){
+            for($j=1; $j<=31; $j++){
+                if($j==9 || $j==10 || $j==11 || $j==14 || $j>15 && $j<22 || $j==28) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+                
+            work.innerHTML = "";
+            work.innerHTML += "<li>09.07  토요휴업일</li>";
+            for($i=10; $i<=31; $i++){
+                if($i == 14 || $i==21 || $i==28) work.innerHTML += "<li>09."+$i+"  토요휴업일</li>";
+                if($i == 10) work.innerHTML += "<li>09."+$i+"  1학년영어듣기평가</li>";
+                if($i>=16 && $i<=18) work.innerHTML += "<li>09."+$i+"  추석연휴</li>";
+                if($i==19 || $i==20) work.innerHTML += "<li>09."+$i+"  재량휴업일</li>";
+                if($i==11) work.innerHTML += "<li>09."+$i+"  2학년영어듣기평가</li>";
+            }
+        }
+        if(number == 10){
+            for($j=1; $j<=31; $j++){
+                if($j>2 && $j<6 || $j==9 || $j==12 || $j==19 || $j>22 && $j<27) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+                
+            work.innerHTML = "";
+            work.innerHTML += "<li>10.03  개천절</li>";
+            work.innerHTML += "<li>10.04  재량휴업일</li>";
+            work.innerHTML += "<li>10.05  토요휴업일</li>";
+            work.innerHTML += "<li>10.09  한글날</li>";
+
+            for($i=10; $i<=31; $i++){
+                if($i == 12 || $i==19 || $i==26) work.innerHTML += "<li>10."+$i+"  토요휴업일</li>";
+                if($i == 23 || $i==24) work.innerHTML += "<li>10."+$i+"  중간고사</li>";
+                if($i==25) work.innerHTML += "<li>10."+$i+"  입학전형</li>", work.innerHTML += "<li>10."+$i+"  현장체험학습</li>";
+            }
+        }
+        if(number == 11){
+            for($j=1; $j<=31; $j++){
+                if($j==2 || $j==9 || $j==16 || $j==23 || $j==30) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+                
+            work.innerHTML = "";
+            for($i=1; $i<=9; $i++){
+                if($i == 2 || $i==9) work.innerHTML += "<li>11.0"+$i+"  토요휴업일</li>";
+            }
+            for($i=10; $i<=31; $i++){
+                if($i == 16 || $i==23 || $i==30) work.innerHTML += "<li>11."+$i+"  토요휴업일</li>";
+            }
+            
+        }
+        if(number == 12){
+            for($j=1; $j<=31; $j++){
+                if($j==7 || $j==14 || $j>17 && $j<22 || $j==25 || $j==28) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+                
+            work.innerHTML = "";
+            work.innerHTML += "<li>12.07  토요휴업일</li>";
+            for($i=10; $i<=31; $i++){
+                if($i == 14 || $i==21 || $i==28) work.innerHTML += "<li>12."+$i+"  토요휴업일</li>";
+                if($i>17 && $i<21) work.innerHTML += "<li>12."+$i+"  기말고사(1,2학년)</li>";
+                if($i==25) work.innerHTML += "<li>12."+$i+"  크리스마스</li>", work.innerHTML += "<li>12."+$i+"  기독탄신일(성탄절)</li>";
+            }
+        }
+    }
+    
 }
+
+
 function minus(){
     const current = parseInt(month.textContent);
+    var j;
     if(current - 1 < 1){
-        month.textContent = current +11 + "월";
+        month.textContent = current + "월";
     }
-    else
+    else{
         month.textContent = current -1 + "월";
+        number = number - 1;
+        if(number == 1){
+            for($j=1; $j<=31; $j++)
+                document.getElementById("num"+$j).style.background = "none";
+            document.getElementById("num1").style.background = "#4DB02C";
+            document.getElementById("num9").style.background = "#4DB02C";
+            work.innerHTML = "";
+            work.innerHTML += "<li>01.01  신정</li>";
+            work.innerHTML += "<li>01.09  종업식,졸업식</li>";
+        }
+        if(number == 2){
+            for($j=1; $j<=31; $j++){
+                if($j>8 && $j<13) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+                
+            work.innerHTML = "";
+            work.innerHTML += "<li>02.09  설날연휴</li>";
+            work.innerHTML += "<li>02.10  설날</li>";
+            work.innerHTML += "<li>02.11  설날연휴</li>";
+            work.innerHTML += "<li>02.12  대체휴일</li>";
+        }
+        if(number == 3){
+            for($j=1; $j<=31; $j++){
+                if($j==1 || $j==2 || $j==4 || $j==9 || $j==16 || $j==23 || $j==30) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+            work.innerHTML = "";
+            for($i=1; $i<=9; $i++){
+                if($i == 2 || $i==9) work.innerHTML += "<li>03.0"+$i+"  토요휴업일</li>";
+                if($i==1) work.innerHTML += "<li>03.0"+$i+"  3.1절</li>";
+                if($i==4) work.innerHTML += "<li>03.0"+$i+"  입학식</li>";
+            }
+            for($i=10; $i<=31; $i++){
+                if($i==16 || $i==23 || $i==30) work.innerHTML += "<li>03."+$i+"  토요휴업일</li>";
+            }
+        }
+        if(number == 4){
+            for($j=1; $j<=31; $j++){
+                if($j==6 || $j>8 && $j<12 || $j==13 || $j==20 || $j>24 && $j<28) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+            work.innerHTML = "";
+            work.innerHTML += "<li>04.06  토요휴업일</li>";
+            work.innerHTML += "<li>04.09  1학년 영어듣기평가</li>";
+
+            for($i=10; $i<=31; $i++){
+                if($i==11) work.innerHTML += "<li>04."+$i+"  2학년 영어듣기평가</li>";
+                if($i==13 || $i==20 || $i==27) work.innerHTML += "<li>04."+$i+"  토요휴업일</li>";
+                if($i==24 || $i==25) work.innerHTML += "<li>04."+$i+"  중간고사</li>";
+            }
+        }
+        if(number == 5){
+            for($j=1; $j<=31; $j++){
+                if($j>3 && $j<7 || $j==11 || $j==15 || $j==18 || $j>19 && $j<26) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+            work.innerHTML = "";
+            work.innerHTML += "<li>05.04  토요휴업일</li>";
+            work.innerHTML += "<li>05.05  어린이날</li>";
+            work.innerHTML += "<li>05.06  대체휴일</li>";
+            for($i=10; $i<=31; $i++){
+                if($i==15) work.innerHTML += "<li>07."+$i+"  석가탄신일(부처님오신날)</li>";
+                if($i==11 || $i==18 || $i==25) work.innerHTML += "<li>05."+$i+"  토요휴업일</li>";
+                if($i>=20 && $i<=25) work.innerHTML += "<li>05."+$i+"  현장체험학습</li>";
+            }
+        }
+        if(number == 6){
+            for($j=1; $j<=31; $j++){
+                if($j==1 || $j>5 && $j<9 || $j==15 || $j==22 || $j==29) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+            work.innerHTML = "";
+            work.innerHTML += "<li>06.01  토요휴업일</li>";
+            work.innerHTML += "<li>06.06  현충일</li>";
+            work.innerHTML += "<li>06.07  개교기념일</li>";
+            work.innerHTML += "<li>06.08  토요휴업일</li>";
+            for($i=10; $i<=31; $i++){
+                if($i==15 || $i==22 || $i==19) work.innerHTML += "<li>06."+$i+"  토요휴업일</li>";
+            }
+        }
+        if(number == 7){
+            for($j=1; $j<=31; $j++){
+                if($j>0 && $j<4 || $j==6 || $j==10 || $j==13 || $j==20 || $j==19 || $j>21 && $j<32) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+            document.getElementsByName
+            work.innerHTML = "";
+            for($i=1; $i<=9; $i++){
+                if($i <= 3) work.innerHTML += "<li>07.0"+$i+"  기말고사</li>";
+                if($i==6) work.innerHTML += "<li>07.0"+$i+"  토요휴업일</li>";
+            }
+            for($i=10; $i<=31; $i++){
+                if($i==10) work.innerHTML += "<li>07."+$i+"  직업기초능력평가(3학년)</li>";
+                if($i==13 || $i==20 || $i==27) work.innerHTML += "<li>07."+$i+"  토요휴업일</li>";
+                work.innerHTML += "<li>07."+$i+"  여름방학</li>";
+            }
+        }
+        if(number == 8){
+            for($j=1; $j<=31; $j++){
+                if($j>0 && $j<18 || $j==19 || $j==24 || $j==31) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+            work.innerHTML = "";
+            for($i=1; $i<=9; $i++){
+                work.innerHTML += "<li>08.0"+$i+"  여름방학</li>";
+                if($i == 3) work.innerHTML += "<li>08.0"+$i+"  토요휴업일</li>";
+            }
+            for($i=10; $i<=31; $i++){
+                if($i == 10 || $i==17 || $i==24 || $i==31) work.innerHTML += "<li>08."+$i+"  토요휴업일</li>";
+                if($i == 15) work.innerHTML += "<li>08."+$i+"  광복절</li>";
+                if($i<19) work.innerHTML += "<li>08."+$i+"  여름방학</li>";
+                if($i==19) work.innerHTML += "<li>08."+$i+"  개학식</li>";
+            }
+        }
+        if(number == 9){
+            for($j=1; $j<=31; $j++){
+                if($j==9 || $j==10 || $j==11 || $j==14 || $j>15 && $j<22 || $j==28) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+            work.innerHTML = "";
+            work.innerHTML += "<li>09.07  토요휴업일</li>";
+            for($i=10; $i<=31; $i++){
+                if($i == 14 || $i==21 || $i==28) work.innerHTML += "<li>09."+$i+"  토요휴업일</li>";
+                if($i == 10) work.innerHTML += "<li>09."+$i+"  1학년영어듣기평가</li>";
+                if($i>=16 && $i<=18) work.innerHTML += "<li>09."+$i+"  추석연휴</li>";
+                if($i==19 || $i==20) work.innerHTML += "<li>09."+$i+"  재량휴업일</li>";
+                if($i==11) work.innerHTML += "<li>09."+$i+"  2학년영어듣기평가</li>";
+            }
+        }
+        if(number == 10){
+            for($j=1; $j<=31; $j++){
+                if($j>2 && $j<6 || $j==9 || $j==12 || $j==19 || $j>22 && $j<27) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+            work.innerHTML = "";
+            work.innerHTML += "<li>10.03  개천절</li>";
+            work.innerHTML += "<li>10.04  재량휴업일</li>";
+            work.innerHTML += "<li>10.05  토요휴업일</li>";
+            work.innerHTML += "<li>10.09  한글날</li>";
+
+            for($i=10; $i<=31; $i++){
+                if($i == 12 || $i==19 || $i==26) work.innerHTML += "<li>10."+$i+"  토요휴업일</li>";
+                if($i == 23 || $i==24) work.innerHTML += "<li>10."+$i+"  중간고사</li>";
+                if($i==25) work.innerHTML += "<li>10."+$i+"  입학전형</li>", work.innerHTML += "<li>10."+$i+"  현장체험학습</li>";
+            }
+        }
+        if(number == 11){
+            for($j=1; $j<=31; $j++){
+                if($j==2 || $j==9 || $j==16 || $j==23 || $j==30) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+            work.innerHTML = "";
+            for($i=1; $i<=9; $i++){
+                if($i == 2 || $i==9) work.innerHTML += "<li>11.0"+$i+"  토요휴업일</li>";
+            }
+            for($i=10; $i<=31; $i++){
+                if($i == 16 || $i==23 || $i==30) work.innerHTML += "<li>11."+$i+"  토요휴업일</li>";
+            }
+            
+        }
+        if(number == 12){
+            for($j=1; $j<=31; $j++){
+                if($j==7 || $j==14 || $j>17 && $j<22 || $j==25 || $j==28) document.getElementById("num"+$j).style.background = "#4DB02C";
+                else document.getElementById("num"+$j).style.background = "none";
+            }
+            work.innerHTML = "";
+            work.innerHTML += "<li>12.07  토요휴업일</li>";
+            for($i=10; $i<=31; $i++){
+                if($i == 14 || $i==21 || $i==28) work.innerHTML += "<li>12."+$i+"  토요휴업일</li>";
+                if($i>17 && $i<21) work.innerHTML += "<li>12."+$i+"  기말고사(1,2학년)</li>";
+                if($i==25) work.innerHTML += "<li>12."+$i+"  크리스마스</li>", work.innerHTML += "<li>12."+$i+"  기독탄신일(성탄절)</li>";
+            }
+        }
+    }
+        
+
 }
+
